f5c4fc2145fcfdfdd908406358c027d164b2356c
Andrew Comminos <andrewcomminos@gmail.com>
2014-07-14 16:17:36 -0700

    More model checks for text message logging.

--- app/src/main/java/com/morlunk/mumbleclient/channel/ChannelChatFragment.java
+++ app/src/main/java/com/morlunk/mumbleclient/channel/ChannelChatFragment.java
@@ -322,17 +322,17 @@ public class ChannelChatFragment extends JumbleServiceFragment implements ChatTa
 
                 if (message.getChannels() != null && !message.getChannels().isEmpty()) {
                     Channel currentChannel = message.getChannels().get(0);
-                    if (currentChannel.getName() != null) {
+                    if (currentChannel != null && currentChannel.getName() != null) {
                         targetMessage = getContext().getString(R.string.chat_message_to, message.getActorName(), currentChannel.getName());
                     }
                 } else if (message.getTrees() != null && !message.getTrees().isEmpty()) {
                     Channel currentChannel = message.getTrees().get(0);
-                    if (currentChannel.getName() != null) {
+                    if (currentChannel != null && currentChannel.getName() != null) {
                         targetMessage = getContext().getString(R.string.chat_message_to, message.getActorName(), currentChannel.getName());
                     }
                 } else if (message.getUsers() != null && !message.getUsers().isEmpty()) {
                     User user = message.getUsers().get(0);
-                    if (user.getName() != null) {
+                    if (user != null && user.getName() != null) {
                         targetMessage = getContext().getString(R.string.chat_message_to, message.getActorName(), user.getName());
                     }
                 } else {
